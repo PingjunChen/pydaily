ae10308de06a2cfcdade5ad187dc6c788f3f6595
PingjunChen <chenpingjun@gmx.com>
2017-11-02 17:30:55 -0400

    set python3.6 to python3.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.4',
     'Topic :: Scientific/Engineering',
 ]
 

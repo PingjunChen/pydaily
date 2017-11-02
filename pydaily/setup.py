8bc812d602cc30319ba03cf665809daa0fd84fef
pingpong <codingpingjun@gmail.com>
2017-11-02 16:33:58 -0400

    1. add setup.py for all subpackages.
    2. add tests folder for each subpackage.
    
    3. rename batch_reanme

---
+++ pydaily/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, pdb
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('pydaily', parent_package, top_path)
+
+    config.add_subpackage('contours')
+    config.add_subpackage('filesystem')
+    config.add_subpackage('images')
+    config.add_subpackage('plots')
+    config.add_subpackage('slides')
+
+    # Add test directories
+    from os.path import isdir, dirname, join
+    rel_isdir = lambda d: isdir(join(curpath, d))
+
+    curpath = join(dirname(__file__), './')
+    subdirs = [join(d, 'tests') for d in os.listdir(curpath) if rel_isdir(d)]
+    subdirs = [d for d in subdirs if rel_isdir(d)]
+
+    for test_dir in subdirs:
+        config.add_data_dir(test_dir)
+
+    return config
+
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+
+    config = configuration(top_path='').todict()
+    setup(**config)

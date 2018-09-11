e6932f09e0c5a091372bf6c587c1edc54107fe0a
PingjunChen <chenpingjun@gmx.com>
2018-09-11 17:28:18 -0400

    remove plot subpackage

--- pydaily/setup.py
+++ pydaily/setup.py
@@ -13,7 +13,6 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('filesystem')
     config.add_subpackage('image')
     config.add_subpackage('metric')
-    config.add_subpackage('plots')
 
     # Add test directories
     from os.path import isdir, dirname, join
